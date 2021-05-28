554c23b037e6c27ded83870e04800e6ca83899d9
wenjing.liu <liuwenjing.12@163.com>
2021-05-28 15:28:14 +0800

    replace the moudule

--- src/main/java/org/wj/cloud/WjCloudApplication.java
+++ src/main/java/org/wj/cloud/WjCloudApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by wenjing.liu on 2021/5/27 in J1.
+ * Hello world!
  *
- * @author wenjing.liu
  */
 @SpringBootApplication
-public class WjCloudApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        SpringApplication.run(WjCloudApplication.class, args);
+public class WjCloudApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(WjCloudApplication.class,args);
     }
 }
--- src/main/java/org/wj/cloud/WjCloudController.java
+++ src/main/java/org/wj/cloud/WjCloudController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by wenjing.liu on 2021/5/27 in J1.
+ * Created by wenjing.liu on 2021/5/28 in J1.
  *
  * @author wenjing.liu
  */
